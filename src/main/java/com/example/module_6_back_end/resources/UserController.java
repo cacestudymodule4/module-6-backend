719397916f280401de0096352ff9d31dccb07f90
pham-van-man <phamvanman.com.vn@gmail.com>
2024-11-08 10:11:06 +0700

    first commit

---
+++ src/main/java/com/example/module_6_back_end/resources/UserController.java
@@ -0,0 +1,26 @@
+package com.example.module_6_back_end.resources;
+
+import com.example.module_6_back_end.dto.UserDTO;
+import com.example.module_6_back_end.model.User;
+import com.example.module_6_back_end.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/api/user-detail")
+    public ResponseEntity<UserDTO> getUserDetail() {
+        User user = userService.getCurrentUser();
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        return ResponseEntity.ok().body(userDTO);
+    }
+}
