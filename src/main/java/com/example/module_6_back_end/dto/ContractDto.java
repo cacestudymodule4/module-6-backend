859a875cf15dd0f7d4e4c4b311f8882bcba7fbea
hieu308 <musicgoddn2023@gmail.com>
2024-11-11 08:46:24 +0700

    hieu up view,delete,add,contract

--- src/main/java/com/example/module_6_back_end/dto/ContractDto.java
+++ src/main/java/com/example/module_6_back_end/dto/ContractDto.java
@@ -1,29 +1,22 @@
 package com.example.module_6_back_end.dto;
 
 import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContractDto {
-  private String  cmd;
-  private Long staffId;
-  private Long term;
-  private Long ground;
-  private LocalDate startDay;
-  private LocalDate endDay;
-  private double price;
-  private double deposit;
-  private String content;
-
-//    staffId: '',
-//    term: '',
-//    ground: '',
-//    startDay: '',
-//    price: '',
-//    endDay: '',
-//    deposit: '',
-//    content:
+    private String cmd;
+    private Long staffId;
+    private Long term;
+    private Long ground;
+    private LocalDate startDay;
+    private LocalDate endDay;
+    private double price;
+    private double deposit;
+    private String content;
 }
