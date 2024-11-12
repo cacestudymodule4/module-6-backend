24de17939fc398057be9d030a62f66bba9ba644a
thanhcong1210 <danglethanhcong4@gmail.com>
2024-11-12 16:58:21 +0700

    cong pull code

--- src/main/java/com/example/module_6_back_end/repository/StaffRepository.java
+++ src/main/java/com/example/module_6_back_end/repository/StaffRepository.java
@@ -2,13 +2,26 @@ package com.example.module_6_back_end.repository;
 
 import com.example.module_6_back_end.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Long> {
-    List<Staff> findByNameContainingIgnoreCase(String name);
+    @Query("SELECT s FROM Staff s WHERE " +
+            "(:codeStaff IS NULL OR s.codeStaff LIKE :codeStaff)" +
+            "AND (:name Is NULL OR s.name LIKE :name)" +
+            "AND (:position IS NULL OR s.position LIKE :position)"
+    )
+    List<Staff> findByCodeStaffOrNameOrPosition(@Param("codeStaff") String codeStaff,
+                                                @Param("name") String name,
+                                                @Param("position") String position);
 
     boolean existsByEmail(String email);
+
+    boolean existsByCodeStaff(String codeStaff);
+
+    boolean existsByPhone(String phone);
 }
