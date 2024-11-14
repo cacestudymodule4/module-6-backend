ae237f85d9fbd44b82ad2229e857a8060e9f5278
thanhcong1210 <danglethanhcong4@gmail.com>
2024-11-14 08:59:43 +0700

    cong pull code 14/12

--- src/main/java/com/example/module_6_back_end/repository/UserRepository.java
+++ src/main/java/com/example/module_6_back_end/repository/UserRepository.java
@@ -7,4 +7,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByPhone(String phone);
+
+    boolean existsByIdentification(String identification);
 }
