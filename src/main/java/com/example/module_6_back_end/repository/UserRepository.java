a82c42a8aa7a340a5befcb102e3a936061fad879
pham-van-man <phamvanman.com.vn@gmail.com>
2024-11-20 10:32:34 +0700

    first commit

--- src/main/java/com/example/module_6_back_end/repository/UserRepository.java
+++ src/main/java/com/example/module_6_back_end/repository/UserRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-
     @Query("SELECT u FROM User u WHERE u.email = :username OR u.username = :username OR u.phone = :username")
     User findByEmailOrUsernameOrPhone(String username);
 }
